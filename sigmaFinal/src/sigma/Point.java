8ed8c3ded864432e1c2bb1124d3d580b0023af8f
Christian Patry <cpatry@moncoyote.com>
2015-03-28 12:20:49 +0100

    class point 2

--- sigmaFinal/src/sigma/Point.java
+++ sigmaFinal/src/sigma/Point.java
@@ -5,13 +5,21 @@ public class Point {
 	int c;
 	int r;
 	
-	int R = 75;
-	int C = 300;
+	static int R = 75;
+	static int C = 300;
 	
 	public int distance(Point p) {
 
 		int rowdist = Math.abs(this.r - p.r);
 		int columndist = Math.min(Math.abs(this.c - p.c),C-Math.abs(this.c - p.c));	
+
+		return columndist + rowdist;
+	}
+	
+	public static int distance(Point p, Point p2) {
+
+		int rowdist = Math.abs(p2.r - p.r);
+		int columndist = Math.min(Math.abs(p2.c - p.c),C-Math.abs(p2.c - p.c));	
 
 		return columndist + rowdist;
 	}
