d94ebea1cd4876b6955e143db37c1108c2595ca3
maxday <got2bex@gmail.com>
2015-03-28 09:59:48 +0100

    final round project setup !

---
+++ sigma/src/sigma/Cell.java
@@ -0,0 +1,17 @@
+package sigma;
+
+public class Cell {
+
+	int x;
+	int y;
+	boolean isHam;
+	
+	public Cell(int x, int y, boolean isHam) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.isHam = isHam;
+	}
+	
+	
+}
---
+++ sigmaFinal/src/sigma/Main.java
@@ -0,0 +1,54 @@
+package sigma;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+		//List<String> l = read("test_round.in");
+	}
+
+	
+	public static List<String> read(final String filename) {
+		
+		List<String> read = new ArrayList<String>();
+		
+		BufferedReader br;
+		try {
+			br = new BufferedReader(new FileReader(filename));
+		} catch (FileNotFoundException e) {
+			return read;
+		}
+	    try {
+	        
+	        String line = br.readLine();
+
+	        while (line != null) {
+	            read.add(line);
+	            line = br.readLine();
+	        }
+	        br.close();
+	    }
+	    catch (IOException e) {
+			return read;
+		}
+	    return read;
+	}
+	
+	/*
+	PrintWriter writer = new PrintWriter("the-file-name.txt", "UTF-8");
+	writer.println("The first line");
+	writer.println("The second line");
+	writer.close();
+	
+	zip -r archive_name.zip src/ 
+	*/
+}
